b42ea10e4afb7e470430d834308f895150ab4ee7
fich146 <marissa.fichera@pnnl.gov>
2023-08-16 18:00:47 -0700

    increase slider displacement tolerance to 5%

--- mcmc_rsf.py
+++ mcmc_rsf.py
@@ -638,8 +638,8 @@ def main():
         pm.Potential("likelihood", loglike(theta))
 
         # seq. mcmc sampler parameters
-        tune = 100
-        draws = 1000
+        tune = 1000
+        draws = 10000
         chains = 2
         cores = 4
 
