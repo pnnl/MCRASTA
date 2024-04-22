84c3d02308f6fcccab968fc9d577808ba5248011
marissafichera <marissa.fichera@pnnl.gov>
2024-04-22 14:30:57 -0700

    plotting p5894 attempt

--- multiprocesstest.py
+++ multiprocesstest.py
@@ -119,7 +119,7 @@ def get_dataset():
 
     # load observed section data and mcmc inference data
     times, mt, vlps, x = pmr.load_section_data(idata_location)
-    idat = pmr.load_inference_data(idata_location, f'{gpl.dirname}_idata')
+    idat = pmr.load_inference_data(idata_location, f'{gpl.dirname}_{gpl.section_id}_idata')
 
     # first plot: mcmc trace with all original data
     # pmr.plot_trace(idata, chain=None)
