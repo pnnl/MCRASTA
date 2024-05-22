5a6acb5f9e8e87bdb57838c8e495f23a778fd01b
marissafichera <marissa.fichera@pnnl.gov>
2024-05-22 09:21:20 -0700

    posterior draws

--- posterior_draws.py
+++ posterior_draws.py
@@ -203,7 +203,7 @@ def save_figs():
         plt.figure(i).savefig(os.path.join(name, f'fig{i}.png'), dpi=300, bbox_inches='tight')
 
 
-def main(pathname, op_file):
+def next(pathname, op_file):
     musims = get_npy_data(pathname, op_file)
     logps1 = get_npy_data(pathname, f'logps_p{gpl.section_id}_0')
     logps2 = get_npy_data(pathname, f'logps_p{gpl.section_id}_1')
@@ -237,5 +237,5 @@ if __name__ == '__main__':
         op = np.array(outputs)
         np.save(pathname, op)
 
-    main(pathname, op)
+    next(pathname, op)
 
