69b7bec6e9eab403b8a23da826a8b43c094d438a
marissafichera <marissa.fichera@pnnl.gov>
2024-05-28 08:20:46 -0700

    testing p5894

--- gplot.py
+++ gplot.py
@@ -7,13 +7,13 @@ import json
 
 class Globals:
     def __init__(self):
-        self.samplename = 'p5756'
+        self.samplename = 'p5894'
         self.mintime = None
         self.maxtime = None
         self.mindisp = None
         self.maxdisp = None
-        self.section_id = '5756001'
-        self.k = 0.0021129
+        self.section_id = '5894001'
+        self.k = 0.00153
         self.lc = 125
         self.rootpath = os.path.join(os.path.expanduser('~'), 'PycharmProjects', 'mcmcrsf_xfiles')
         self.vel_windowlen = None
@@ -27,7 +27,7 @@ class Globals:
         self.sim_name = f'out_{self.ndr}d{self.nch}ch_{self.section_id}'
         self.mu_sim = None
         self.aminbmode = None
-        self.threshold = 0.2
+        self.threshold = 0.0284
         self.nrstep = 1
         self.nrplot = self.nch * self.ndr / self.nrstep   # nch * ndr / nrstep
         self.tcrit = None
@@ -120,7 +120,7 @@ class Globals:
             gpl.nch = js.get('n_chains')
             gpl.ntune = js.get('n_tune')
             vref = js.get('vref')
-            gpl.threshold = 0.2
+            gpl.threshold = 0.0284
 
             priors_info = js.get('prior_mus_sigmas', 'priors info not available')
             mus = priors_info[0]
