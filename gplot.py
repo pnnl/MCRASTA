3187d6dc27b411a5c4e2d5ffbeabf47f2a2a0a22
marissafichera <marissa.fichera@pnnl.gov>
2024-05-29 10:32:41 -0700

    5894 posterior draws test

--- gplot.py
+++ gplot.py
@@ -7,18 +7,18 @@ import json
 
 class Globals:
     def __init__(self):
-        self.samplename = 'p5895'
+        self.samplename = 'p5894'
         self.mintime = None
         self.maxtime = None
         self.mindisp = None
         self.maxdisp = None
-        self.section_id = '5895001'
-        self.k = 0.0012896
+        self.section_id = '5894001'
+        self.k = 0.00153
         self.lc = 125
         self.rootpath = os.path.join(os.path.expanduser('~'), 'PycharmProjects', 'mcmcrsf_xfiles')
-        self.vel_windowlen = 150
-        self.filter_windowlen = 20
-        self.q = 10
+        self.vel_windowlen = 40
+        self.filter_windowlen = 10
+        self.q = 5
         self.ndr = 500000
         self.nch = 4
         self.ntune = None
@@ -27,12 +27,12 @@ class Globals:
         self.sim_name = f'out_{self.ndr}d{self.nch}ch_{self.section_id}'
         self.mu_sim = None
         self.aminbmode = None
-        self.threshold = 0.6
+        self.threshold = 0.02
         self.nrstep = 1
         self.nrplot = self.nch * self.ndr / self.nrstep   # nch * ndr / nrstep
         self.tcrit = None
         self.vmax = None
-        self.dirname = f'out_{self.ndr}d{self.nch}ch'
+        self.dirname = f'~out_{self.ndr}d{self.nch}ch'
         self.msname = f'mu_simsp{self.section_id}'
 
     def make_path(self, *args):
@@ -120,7 +120,7 @@ class Globals:
             gpl.nch = js.get('n_chains')
             gpl.ntune = js.get('n_tune')
             vref = js.get('vref')
-            gpl.threshold = 0.6
+            gpl.threshold = 0.02
 
             priors_info = js.get('prior_mus_sigmas', 'priors info not available')
             mus = priors_info[0]
