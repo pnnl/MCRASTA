0c00699549a89f0a82045091096903019af7b879
marissafichera <marissa.fichera@pnnl.gov>
2024-06-02 23:54:05 -0700

    rerun p5894, no neg vlps

--- posterior_draws.py
+++ posterior_draws.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
     t, mutrue, vlps, x = load_section_data()
     parent_dir = gpl.get_musim_storage_folder()
 
-    num_draws = 2000
+    num_draws = 4000
     # a, b, Dc, mu0 = get_model_values(idata)
     drawed_vars = draw_from_posteriors(ndraws=num_draws)
 
