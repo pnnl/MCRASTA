6884735bf201454c1d1321896f654b175b56e3f3
marissafichera <marissa.fichera@pnnl.gov>
2024-06-07 07:59:19 -0700

    p5895001, sigma and Dc weibull

--- run_plotting_scripts.py
+++ run_plotting_scripts.py
@@ -5,8 +5,8 @@ from plots import main as mainplot
 
 
 if __name__ == '__main__':
-    # pmr()
-    # cls()
+    pmr()
+    cls()
     post_draws()
     mainplot()
     print('END ALL PLOT SCRIPTS')
